1d4d5b309fc90527780dc13e441aac196cfbf56b
imisi99 <isongrichard234@gmail.com>
2025-01-16 21:30:16 +0100

    adding nested loop

--- ass.java
+++ ass.java
@@ -1,7 +1,7 @@
 public class ass {
 
     public static void main(String[] args) {
-        System.out.println("This is the solution for the nigeria flag");
+        System.out.println("This is using a single for loop");
         for(int i = 0; i <= 47; i ++){
             if (i % 12 == 0) {
                 System.out.println("");
@@ -19,5 +19,19 @@ public class ass {
             }
         }
         System.out.println("");
+
+	System.out.println("This is using a nested for loop");
+        System.out.println("");
+	for(int i = 0; i <= 3; i++){
+	    for(int j = 0; j <= 11; j++){
+		    if (j < 4 | j > 7){
+		        System.out.printf("*");
+		    }
+		    else{
+		        System.out.printf("=");
+		    }
+	    }
+	    System.out.println("");
+	}
     }
 }
